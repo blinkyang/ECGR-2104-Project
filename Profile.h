d74b743b52dfe904b6f7e17352e671d9294d192e
chataolauj <chataolauj@yahoo.com>
2016-04-11 18:29:20 -0400

    Added files via upload

---
+++ Profile.h
@@ -0,0 +1,22 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+#include <iostream>
+
+using namespace std;
+
+class Profile
+{
+    public:
+        Profile();
+        Profile(string, string, int, int);
+        void display_info();
+
+    private:
+        string user_name;
+        string level;
+        int energy_level;
+        int steps;
+};
+
+#endif // PROFILE_H
