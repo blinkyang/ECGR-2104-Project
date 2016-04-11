d98d70c21c25e2f4fc6ca94a0f78b8d735a1c30d
chataolauj <chataolauj@yahoo.com>
2016-04-11 18:30:47 -0400

    Added files via upload

---
+++ Profile.cpp
@@ -0,0 +1,27 @@
+#include "Profile.h"
+#include <iostream>
+
+using namespace std;
+
+Profile::Profile()
+{
+    user_name = " ";
+    level = "Terrestrial";
+    energy_level = 15;
+    steps = 0;
+}
+
+Profile::Profile(string un, string curr_level, int en_lev, int num_steps)
+{
+    user_name = un;
+    level = curr_level;
+    energy_level = en_lev;
+    steps = num_steps;
+}
+
+void Profile::display_info()
+{
+    cout << endl << "--------------------------------------------------------------------------" << endl
+         << "Hello " << user_name << "!" << "\tLevel: " << level << "\tEnergy Level: " << energy_level << "\tSteps: " << steps
+         << endl << "--------------------------------------------------------------------------" << endl;
+}
