b97d0aa3455ab5ff25549cc87ae0a6291a5951ff
adang4 <andydang120@gmail.com>
2016-04-18 16:41:56 -0400

    Update Loadsave.h

--- Loadsave.h
+++ Loadsave.h
@@ -1,6 +1,16 @@
 #IFNDEF 'Loadsave.h'
 #DEFINE 'Loadsave.h'
 
-
+  class Save {
+    private:
+      char name[20];
+      string level;
+      unsigned int energy;
+      int steps;
+    public:
+      Save();
+      Save(string n, string l, unsigned int e, int s);
+      Save(Save& in_obj);
+  }
 
 #ENDIF
