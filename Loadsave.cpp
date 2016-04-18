fb3074fb808fae420b10e204c1e228c8c6d5a0e0
adang4 <andydang120@gmail.com>
2016-04-18 14:59:59 -0400

    Create Loadsave.cpp

---
+++ Loadsave.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Save {
+    private:
+        char name[20];
+    public:
+        Save();
+        Save(char& n, 
+};
+
+int main () {
+
+
+return 0;
+}
