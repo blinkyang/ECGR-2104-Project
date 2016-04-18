67dd7bc83679a3f26f8747e0faa94b28395a5650
adang4 <andydang120@gmail.com>
2016-04-18 14:25:25 -0400

    Update Profile.h

--- Profile.h
+++ Profile.h
@@ -1,12 +1,6 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
-namespace boost {
-namespace serialization{
-class access;
-}    
-}
-
 class Profile
 {
     public:
@@ -15,19 +9,19 @@ class Profile
         void display_info();
 
     private:
+        friend class boost::serialization::access;
+        template<class Archive>
+        void serialize(Archive& ar, const unsigned int version)
+        {
+            ar& user_name;
+            ar& level;
+            ar& energy_level;
+            ar& steps;
+        }
         string user_name;
         string level;
         signed int energy_level;
         int steps;
-        
-        friend class boost::serialization::access;
-        
-        template<typename Archive>
-        
-        void serialize(Archive& ar, const unsigned version) {
-            ar & user_name & level & energy_level;
-        }
-        }
 };
 
 #endif // PROFILE_H
